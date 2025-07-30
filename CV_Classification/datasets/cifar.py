de1bdd8477db2beeee8b8c56c8e3043a23886f1c
sunmin0505 <sunmin_kim@snu.ac.kr>
2025-07-30 18:05:16 +0900

    Update cifar.py

--- CV_Classification/datasets/cifar.py
+++ CV_Classification/datasets/cifar.py
@@ -1,36 +1,56 @@
 from torchvision.datasets import CIFAR10
 from torchvision import transforms
+from torch.utils.data import random_split
 from .augmentation import get_augmentation, get_normalize
+import torch
 
-def get_cifar10_dataset(split: str, aug_level: str = "default"):
+_SPLIT_CACHE = None
+
+def get_cifar10_dataset(split: str, aug_level: str = "default", val_ratio: float = 0.1):
     """
-    CIFAR-10 train/test 데이터셋 반환
+    CIFAR-10 train/val/test 데이터셋 반환
 
     Args:
-        split (str): 'train' 또는 'test'
+        split (str): 'train', 'val', 'test'
         aug_level (str): augmentation 수준 ('default', 'weak', 'strong')
-                         test split에서는 무시됨
+        val_ratio (float): train-validation 비율
 
     Returns:
-        torchvision.datasets.CIFAR10 객체
+        CIFAR10 또는 Subset 객체
     """
-    assert split in ['train', 'test'], "split must be 'train' or 'test'"
+    assert split in ['train', 'val', 'test'], "split must be 'train', 'val', or 'test'"
 
-    is_train = (split == 'train')
-
-    if is_train:
-        transform = get_augmentation(aug_level)
-    else:
+    if split == 'test':
         transform = transforms.Compose([
             transforms.ToTensor(),
             get_normalize()
         ])
+        return CIFAR10(root='./data', train=False, transform=transform, download=True)
 
-    dataset = CIFAR10(
+    # train or val
+    full_dataset = CIFAR10(
         root='./data',
-        train=is_train,
-        transform=transform,
+        train=True,
+        transform=get_augmentation(aug_level),
         download=True
     )
 
-    return dataset
+    val_size = int(len(full_dataset) * val_ratio)
+    train_size = len(full_dataset) - val_size
+
+    global _SPLIT_CACHE
+    if _SPLIT_CACHE is None:
+        generator = torch.Generator().manual_seed(42)
+        _SPLIT_CACHE = random_split(full_dataset, [train_size, val_size], generator=generator)
+
+    train_set, val_set = _SPLIT_CACHE
+
+    if split == 'train':
+        return train_set
+    else:
+        # val에는 augmentation 제거
+        val_set.dataset.transform = transforms.Compose([
+            transforms.ToTensor(),
+            get_normalize()
+        ])
+        return val_set
