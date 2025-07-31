8120caf728a103995b27f155ca1adb7995937ad1
sunmin0505 <sunmin_kim@snu.ac.kr>
2025-07-31 09:39:30 +0900

    Update config.py

--- CV_Classification/config.py
+++ CV_Classification/config.py
@@ -1,13 +1,13 @@
 configs = {
     "resnet50": {
-        "epochs": 100,
+        "epochs": 50,
         "batch_size": 64,
         "learning_rate": 0.001,
         "num_classes": 10,
         "num_workers": 4
     },
     "vit_small_patch16_224": {
-        "epochs": 100,
+        "epochs": 50,
         "batch_size": 64,
         "learning_rate": 0.001,
         "num_classes": 10,
